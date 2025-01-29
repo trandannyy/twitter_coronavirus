0f0feb3e1d867341e83349e3796abd202dc316cb
dantran <dantran@cmc.edu>
2025-01-29 11:10:06 -0800

    finished task 0

--- src/map.py
+++ src/map.py
@@ -37,6 +37,7 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
+counter_country = defaultdict(lambda: Counter())
 
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
@@ -60,10 +61,17 @@ with zipfile.ZipFile(args.input_path) as archive:
                 # search hashtags
                 for hashtag in hashtags:
                     lang = tweet['lang']
+                    if tweet.get('place') and tweet['place'].get('country_code'):
+                        country_code = tweet['place']['country_code']
+                    else:
+                        country_code = 'unknown'
                     if hashtag in text:
                         counter_lang[hashtag][lang] += 1
+                        counter_country[hashtag][country_code] += 1
                     counter_lang['_all'][lang] += 1
+                    counter_country['_all'][country_code] += 1
 
+                
 # open the outputfile
 try:
     os.makedirs(args.output_folder)
@@ -72,7 +80,10 @@ except FileExistsError:
 output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
 
 output_path_lang = output_path_base+'.lang'
+output_path_country = output_path_base+'.country'
 print('saving',output_path_lang)
+print('saving', output_path_country)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
-
+with open(output_path_country,'w') as f:
+    f.write(json.dumps(counter_country))
